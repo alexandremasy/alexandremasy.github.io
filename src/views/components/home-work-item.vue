cf107d79aa6af1f779217c9df091674b8576f3db
Alexandre Masy <alexandre@infinity-mobile.io>
2021-06-19 17:28:33 +0200

    Layout the work

---
+++ src/views/components/home-work-item.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="work-item">
+
+    <div class="work-item__part work-item__content">
+      <div class="work-item__intro">
+        <div class="work-item__i">01</div>
+        <div class="work-item__n">•</div>
+        <div class="work-item__n">03</div>
+      </div>
+
+      <div class="work-item__primary">
+        <div class="work-item__title">{{ title }}</div>
+        <div class="work-item__description">{{ description }}</div>
+
+        <a 
+          :href="href"
+          class="work-item__cta link -teal">{{ cta }}</a>
+      </div>
+    </div>
+
+    <figure class="work-item__part work-item__figure">
+      <img class="work-item__image" :src="figure" :alt="title">
+    </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWorkItem',
+
+  props: {
+    cta: {
+      type:String
+    },
+
+    description: {
+      type: String
+    },
+
+    figure: {
+      type: String
+    },
+
+    href: {
+      type: String
+    },
+
+    title: {
+      type: String
+    },
+  }
+}
+</script>
---
+++ src/views/components/home-work.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="view-home-work">
+    <div class="view-home-work__intro">
+      <div class="container">
+        <div class="view-home-work__subtitle">What’s up men?</div>
+        <h2 class="view-home-work__title">My Daily Pixel Work</h2>
+      </div>
+    </div>
+
+    <div class="view-home-work__content">
+      <div class="container">
+        <home-work-item
+          v-for="(c, i) in children"
+          :key="i"
+          v-bind="c" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HomeWorkItem from './home-work-item'
+
+export default {
+  name: 'HomeWork',
+
+  components: {
+    HomeWorkItem,
+  },
+
+  data(){
+    return {
+      children: [
+        {
+          title: 'Sayl',
+          description: 'Branding - Product design - Lead Frontend & Architect',
+          cta: 'Onboard for a journey',
+          href:'https://sayl.cloud',
+          figure: 'statics/images/projects/sayl.svg'
+        },
+        {
+          title: 'Shopitag',
+          description: 'Branding - UX & UI Design - Frontend development',
+          cta: 'Everyone onboard',
+          href:'https://shopitag.com',
+          figure: 'statics/images/projects/shopitag.svg'
+        },
+        {
+          title: 'Spices',
+          description: 'One men show',
+          cta: 'Spice up your life',
+          href:'https://spices.alexandremasy.com',
+          figure: 'statics/images/projects/spices.svg'
+        },
+      ]
+    }
+  }
+}
+</script>
